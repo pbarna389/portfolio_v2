ef98656ad7a354b0d45cbe6dc31c0e193b6d59a2
PB <pbarna389@gmail.com>
2025-03-03 23:35:32 +0100

    05/form - moved related text to it's own component

--- src/layout/Contact/components/ContactForm/ContactForm.tsx
+++ src/layout/Contact/components/ContactForm/ContactForm.tsx
@@ -1,4 +1,4 @@
-import { Form } from './components'
+import { ContactText, Form } from './components'
 
 import './ContactForm.scss'
 
@@ -6,11 +6,7 @@ export const ContactForm = () => {
 	return (
 		<div className="contact-wrapper">
 			<Form />
-			<p className="contact-text">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, obcaecati quia ratione
-				necessitatibus aperiam magnam, sequi at expedita ab consequuntur incidunt officiis corporis,
-				nemo sit molestiae dignissimos cum et impedit.
-			</p>
+			<ContactText />
 		</div>
 	)
 }
--- src/layout/Contact/components/ContactForm/components/index.ts
+++ src/layout/Contact/components/ContactForm/components/index.ts
@@ -1,2 +1,3 @@
 export { ContactFormError } from './ContactFormError'
+export { ContactText } from './ContactText'
 export { Form } from './Form'
