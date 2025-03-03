eabe80ad436afa34a8d4435ea68fd9bbcdbec708
PB <pbarna389@gmail.com>
2025-03-03 15:24:07 +0100

    03/navbar - added useEffect for smooth transitioning

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,3 +2,4 @@ export const CAPTCHA_KEY = import.meta.env.VITE_RECAPTCHA_SITE_KEY
 export const EMAIL_JS_SERVICE_ID = import.meta.env.VITE_EMAIL_SERVICE_ID
 export const EMAIL_JS_TEMPLATE_ID = import.meta.env.VITE_EMAIL_TEMPLATE_ID
 export const EMAIL_JS_USER_ID = import.meta.env.VITE_EMAIL_PUBLIC_KEY
+export const DEVELOPMENT_MODE = import.meta.env.VITE_DEVELOPMENT_MODE
